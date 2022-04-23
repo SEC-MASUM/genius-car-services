fd3fe735c0e537cf672d89d15086cb8ae1fa5720
SEC-MASUM <masum.jess42@gmail.com>
2022-04-24 00:29:36 +0600

    refactor :  create useServices hook to load data

---
+++ src/Hooks/useServices.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useServices = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:5000/service";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+        // console.log(data);
+      });
+  }, []);
+  return [services, setServices];
+};
+
+export default useServices;
