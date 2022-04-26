3bc161ddfd54512c5d5314282339ea22cce20403
SEC-MASUM <masum.jess42@gmail.com>
2022-04-26 06:45:28 +0600

    feat: controlled address field

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import useServiceDetail from "../../../Hooks/useServiceDetail";
 
 const Checkout = () => {
+  const { serviceId } = useParams();
+  const [service] = useServiceDetail(serviceId);
+
+  const [user, setUser] = useState({
+    name: "Masum Billah",
+    email: "masum@car.com",
+    address: "Jashore, Bangladesh",
+    phone: "01711111111",
+  });
+
+  const handleAddressChange = (event) => {
+    console.log(event.target.value);
+    const { address, ...rest } = user;
+    const newAddress = event.target.value;
+    const newUser = { address: newAddress, ...rest };
+    setUser(newUser);
+    console.log(newUser);
+  };
   return (
-    <div>
-      <h2>Please Checkout your booking</h2>
+    <div className="w-50 mx-auto">
+      <h2>Please Order : {service.name}</h2>
+      <form>
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={user.name}
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="email"
+          placeholder="email"
+          value={user.email}
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="service"
+          placeholder="service"
+          value={service.name}
+          required
+        />
+        <br />
+        <input
+          onChange={handleAddressChange}
+          className="w-100 mb-2"
+          type="text"
+          name="address"
+          placeholder="address"
+          value={user.address}
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="phone"
+          placeholder="phone"
+          value={user.phone}
+          required
+        />
+        <br />
+        <input className="btn btn-primary" type="submit" value="Place Order" />
+      </form>
     </div>
   );
 };
